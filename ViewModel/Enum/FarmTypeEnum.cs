2ead5e6316bfceed1e512e8ebc096975777029ad
Suvash Shah Thakuri <sthakuri@users.noreply.github.com>
2019-12-31 16:57:16 -0800

    Layout added

---
+++ ViewModel/Enum/FarmTypeEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ViewModel.Enum
+{
+    public enum FarmTypeEnum
+    {
+        //This is a type of farm that produces only enough food to feed the family.
+        SubsistenceFarm = 1,
+        CommercialFarm = 2,
+        CropFarm = 3,
+        FishFarm = 4,
+        DairyFarm = 5,
+        PoultryFarm = 6,
+        MeatFarm = 7
+    }
+}
---
+++ ViewModel/Enum/ProductCategoryEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ViewModel.Enum
+{
+    public enum ProductCategoryEnum
+    {
+        Food = 1,
+        Fuels = 2,
+        Fibers = 3,
+        RawMaterials = 4
+    }
+}
